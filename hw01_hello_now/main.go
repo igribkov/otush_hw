dec3be02a287cc91b699782a07920f56523226f2
Gribkov Ivan <igribkov@corp.badoo.com>
2020-09-04 02:10:34 +0300

    test

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"log"
+	"time"
+)
+
 func main() {
-	// Place your code here
+	localhostTime := time.Now().Round(0)
+	currentNtpTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Smth goes wrong. Error: \"%v\"", err)
+	}
+
+	//no need to do Round() due to stripMono has been already done into Local()
+	fmt.Printf("current time: %s\nexact time: %s\n", localhostTime.String(), currentNtpTime.Local().String())
 }
